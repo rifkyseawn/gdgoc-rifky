deb179aa38630e126a2dc37616bfdd0ed3ae5bc3
rifkyseawn <141981106+rifkyseawn@users.noreply.github.com>
2024-11-22 17:25:17 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 app.use(
   cors({
-    origin: ["https://gdgoc-rifky-server.vercel.app/"],
+    origin: ["https://gdgoc-rifky.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
